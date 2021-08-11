8fb16051846de96a2a687bf5fc39f5f4e5a09e92
Ilya Maleitin <ilia.maleitin@gmail.com>
2021-08-11 20:28:44 +0300

    Create Models and Controller

---
+++ ToDoApi/ToDoApi/Models/TasksContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApi.Models
+{
+    public class TasksContext : DbContext
+    {
+        public DbSet<TodoItem> Tasks { get; set; }
+        public TasksContext(DbContextOptions<TasksContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>()
+                .Property(b => b.Name)
+                .IsRequired();
+        }
+    }
+}
